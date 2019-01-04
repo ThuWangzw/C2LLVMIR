3c1f1080b5d90cd09217faf6f86f15a6ec3952ac
wonderfulnx <naxinlegend@outlook.com>
2019-01-05 04:57:55 +0800

    添加返回类型为空

--- test/naive.c
+++ test/naive.c
@@ -4,10 +4,13 @@ extern char* puts(char* s);
 int main()
 {
     int a[100];
-    a[9] = 192;
-    // a = 100;
-    // int a[100];
-    // a[0] = 199;
-    // puts("hello,world!");
-    return a[9];
+    int i;
+    a[0] = 1; a[1] = 1;
+    for (i = 2; i < 50; i = i + 1) {
+        a[i] = a[i-1] + a[i-2];
+    }
+    for (i = 0; i < 50; i = i + 1) {
+        1;
+    }
+    return a[5];
 }
