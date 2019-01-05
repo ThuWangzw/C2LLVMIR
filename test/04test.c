1c75ba27e62777a4a35c9f10c6a8aa7ad3f71c16
ThuWangzw <wangzw834@163.com>
2019-01-05 22:01:49 +0800

    修改逻辑或bug

--- test/04test.c
+++ test/04test.c
@@ -1,9 +1,24 @@
+extern char* puts(char* s);
+extern int printf(char* s);
+int max(int a, int b){
+    int c = 0;
+    if(a>b){
+        c = a;
+    } else{
+        c = b;
+    }
+    if((c>0)||(c<0)){
+        printf("then");
+    } else{
+        printf("else");
+    }
+    return c;
+}
 
 int main(){
     char a[100];
-//    char b;
-//    char c;
-//    int d[100];
     a[1];
+    //max(123,456);
+    printf("%d",max(123,456));
     return 0;
 }
